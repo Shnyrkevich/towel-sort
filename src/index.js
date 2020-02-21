03b04629117d81ab1fec054fc45da00dfe79e154
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-02-21 12:45:18 +0300

    feat: realize towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+
+  let mas = [];
+
+  if(!Array.isArray(matrix)){
+    return [];
+  }
+
+  for(let i = 0, counter = 0, j = 0; i < matrix.length; i++){
+    if((i+1)%2==0){
+      j = matrix[i].length-1;
+      while(j >= 0){
+        mas[counter]=matrix[i][j];
+        counter++;
+        j--;
+      }
+    } else {
+      j = 0;
+      while(j < matrix[i].length){
+        mas[counter]=matrix[i][j];
+        counter++;
+        j++;
+      }
+    }
+  }
+  console.log(mas);
+  return mas;
+
+};
